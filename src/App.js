026a023de0190ffc5f8395acd1c01ce00e3bbb32
Ajinkya Gaikwad <ajinkyagaikwad@Ajinkyas-MacBook-Air.local>
2019-03-02 19:18:53 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
+import SignIn from "./Login/SignIn";
+import SignUp from "./Login/SignUp";
 import Home from "./Home";
 
 class App extends Component {
   render() {
-    return <Home />;
+    return <SignUp />;
   }
 }
 
